f23838b08e8ec54bdde368889b9bf08a781c66c5
William Bugenis <wbugenis@gmail.com>
2021-01-31 18:07:30 -0500

    commit tree b87a0e39f701b715d4a39f4c381f52f537870bd3

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,23 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+
+    num_1 = Kernel.rand(1..3)
+    num_2 = Kernel.rand(1..3)
+    num_3 = Kernel.rand(1..3)
+    
+    resp.write "Num_1 = #{num_1}"
+    resp.write "Num_2 = #{num_2}"
+    resp.write "Num_3 = #{num_3}\n"
+
+    if num_1 == num_2 && num_2 == num_3
+      resp.write "You Win"
+    else 
+      resp.write "You Lose"
+    end
+
     resp.finish
+
   end
 
 end
